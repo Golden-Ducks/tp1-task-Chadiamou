af4d4f00afbf4cccadfd1167740ac39c3823a3d7
unknown <moussaouifadia2@gmail.com>
2026-02-28 18:42:54 +0100

    my solution

---
+++ TP1/task1.py
@@ -0,0 +1,9 @@
+import re
+with open("task1.txt", "r") as f:
+    text = f.read()
+text = text.lower()
+text = re.sub(r'\d+', '', text)
+text = re.sub(r'[^\w\s]', '', text)
+text = re.sub(r'\s+','', text).strip()
+text = re.sub(r"[–]", "", text)
+print(text)
