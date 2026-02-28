0775cb10556e15af9385a01f0cb0f485ff1373b1
unknown <moussaouifadia2@gmail.com>
2026-02-28 18:48:14 +0100

    my solution

--- TP1/task.py
+++ TP1/task.py
@@ -5,8 +5,12 @@ def fct(b):
         '10': 'ten'
     }
     for k, d in number_words.items():
-        b = b.replace(k, d)  # Correction : assigner le résultat à b
-    return b
+        b = b.replace(k, d) 
+    punctuation = "!()-[]{};:,<>./?@#$%^&*_~'\""
+    for c in punctuation:
+        b = b.replace(c, "")
+    words = b.split()
+    return words
 sentences = "Today she cooked 4 bourak. Later, she added two chamiyya and 1 pizza."
 sentences2 = "Handling Contractions Isn't Hard."
 sentences3 = "We only had 8 chamiyya, no pizza, and one tea."
@@ -15,6 +19,7 @@ lowercased_sentences = sentences.lower()
 lowercased_sentences2 = sentences2.lower()
 lowercased_sentences3 = sentences3.lower()
 lowercased_sentences4 = sentences4.lower()
+
 a = fct(lowercased_sentences)
 b = fct(lowercased_sentences2)
 c = fct(lowercased_sentences3)
