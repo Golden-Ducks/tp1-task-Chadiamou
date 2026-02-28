32c63a02d7bfd0d2f0698bff2650d824e1466406
unknown <moussaouifadia2@gmail.com>
2026-02-28 18:40:12 +0100

    my solution

---
+++ TP1/task2.py
@@ -0,0 +1,48 @@
+import re
+
+def normalize_numbers(text):
+    number_words = {
+          '0':'zero', '1': 'one', '2': 'two', '3': 'three', '4': 'four',
+        '5': 'five', '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine',
+        '10': 'ten'
+    }
+    for w, d in number_words.items():
+        text = text.replace(w, d)
+    return text
+
+def normalize_text(text):
+    food_terms = {
+        "can't ": 'can not',
+        "i'm": 'i am',
+        "won't": 'will not',
+        "don't": "do  not",
+        "i'll": 'i will',
+        "let's": 'let us',
+        "that's": 'that is',
+        "wasn't": 'was not',
+        "couldn't": 'could not',
+        "didn't": 'did not',
+        "it't": 'it is',
+        "he's": 'he is',
+        "she's": 'she is',
+        "frog's": 'frog it',
+        "there's": 'there is',
+        "we 're": 'we are'
+    }
+    for term, normalized in food_terms.items():
+        text = text.replace(term, normalized)
+    
+    return text
+
+
+with open("task2.txt", "r") as f:
+    text = f.read()
+text = text.lower()
+text = re.sub(r"[^\w\s]", "", text)
+text = re.sub(r"\s+", " ", text).strip()
+text = normalize_numbers(text)
+text = normalize_text(text)
+
+print(text)
+
+
