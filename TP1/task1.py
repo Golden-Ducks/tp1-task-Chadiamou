62d896befc699bc7df674f8f8214a3e280430a4f
unknown <moussaouifadia2@gmail.com>
2026-02-28 18:46:36 +0100

    my solution

--- TP1/task1.py
+++ TP1/task1.py
@@ -4,6 +4,11 @@ with open("task1.txt", "r") as f:
 text = text.lower()
 text = re.sub(r'\d+', '', text)
 text = re.sub(r'[^\w\s]', '', text)
-text = re.sub(r'\s+','', text).strip()
+text = re.sub(r'\s+','', text)
 text = re.sub(r"[–]", "", text)
+punctuation = "!()-[]{};:,<>./?@#$%^&*_~'\""
+for c in punctuation:
+        text = text.replace(c, "")
+
+words = text.split()
 print(text)
